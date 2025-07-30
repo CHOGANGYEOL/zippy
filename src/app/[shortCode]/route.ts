2760da43167a170cdb462bc1acad780909b1ae17
CHOGANGYEOL <rkdduf06@icloud.com>
2025-07-30 11:20:31 +0900

    feat: redirect api

--- src/app/[shortCode]/route.ts
+++ src/app/[shortCode]/route.ts
@@ -4,17 +4,21 @@ import { redirect } from "next/navigation";
 
 export async function GET(
   _: Request,
-  { params }: { params: { shortKey: string } }
+  ctx: { params: Promise<{ shortCode: string }> }
 ) {
-  const { shortKey } = params;
+  const { shortCode } = await ctx.params;
   const result = await db.send(
     new GetCommand({
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: { shortKey },
+      Key: { shortCode },
     })
   );
   const item = result.Item;
-  if (!item || (item.expireAt && Date.now() > item.expireAt)) {
+
+  if (
+    !item ||
+    (item.expireAt && Math.floor(Date.now() / 1000) > item.expireAt)
+  ) {
     return new Response("URL expired or not found", { status: 404 });
   }
   return redirect(item.originalUrl);
